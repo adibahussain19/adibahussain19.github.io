5bcbca2428745d7652625dbb885ed517757cd12b
adibahussain19 <adibahussain19@gmail.com>
2024-09-07 23:04:49 -0400

    creating writing schema

--- backend/app.js
+++ backend/app.js
@@ -19,12 +19,16 @@ const MONGO_URI = process.env.MONGO_URI.replace('${DB_PASSWORD}', process.env.DB
 
 
 // MongoDB connection
-mongoose.connect(MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
+mongoose.connect(MONGO_URI)
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.error("MongoDB connection error:", err));
+
+//   mongoose.connect(MONGO_URI, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   })
+//     .then(() => console.log("MongoDB connected"))
+//     .catch((err) => console.error("MongoDB connection error:", err));
 
 // Sample route
 app.get("/", (req, res) => {
