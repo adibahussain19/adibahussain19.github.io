3d6ca493ee2c78c63f1327c32b7cea2575731009
Adiba Hussain <adibahussain@mac.myfiosgateway.com>
2024-10-09 01:20:40 -0400

    fixing dependencies

--- backend/app.js
+++ backend/app.js
@@ -51,6 +51,13 @@ app.use((error, req, res, next) => {
   res.status(status).json({ message: message });
 });
 
+//Static file
+app.use(express.static(path.join(__dirname, '../frontend/dist')));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
+});
+
+
 // Start the server
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
